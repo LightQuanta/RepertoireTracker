e69e3c55f51dcf6164975c73f1feaef4c4059dd6
Light_Quanta <zch22248@foxmail.com>
2026-05-15 10:21:11 +0800

    chore: 配置ElementPlus语言

--- src/plugins/vue-app.ts
+++ src/plugins/vue-app.ts
@@ -1,7 +1,14 @@
 import type { App } from 'vue'
 import { ID_INJECTION_KEY, ZINDEX_INJECTION_KEY } from 'element-plus'
+import ElementPlus from 'element-plus'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 export default function (app: App) {
+  app.use(ElementPlus, {
+    // TODO 能否实现语言选择？
+    locale: zhCn,
+  })
+
   app.provide(ID_INJECTION_KEY, { prefix: 0, current: 0 })
   app.provide(ZINDEX_INJECTION_KEY, { current: 0 })
 }
